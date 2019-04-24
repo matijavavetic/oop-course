00870e3718c3a2ce45eaffebbf2dfcf2be008c1b
matijavavetic <47482084+matijavavetic@users.noreply.github.com>
2019-04-24 13:22:02 +0200

    Created user class which includes find user methods

--- gallery/admin/includes/admin_content.php
+++ gallery/admin/includes/admin_content.php
@@ -1,7 +1,27 @@
+
 <div class="container-fluid">
 
     <!-- Page Heading -->
     <div class="row">
+
+
+        <?php
+        include("init.php");
+
+
+        $result_set = User::find_all_users();
+
+        while($row = mysqli_fetch_array($result_set)) {
+
+            echo $row['username'] . "<br>";
+
+        }
+
+        /*$found_user = User::find_users_by_id(1);
+
+        echo $found_user['username'];
+*/
+        ?>
         <div class="col-lg-12">
             <h1 class="page-header">
                 ADMIN
